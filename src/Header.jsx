e5b90eb3e3ae40bba791c1a058d2f2ab7aaa12d0
Keith Gaines <patrick.keith.gaines@gmail.com>
2023-01-27 09:18:16 -0600

    changed navbar content

--- src/Header.jsx
+++ src/Header.jsx
@@ -11,30 +11,25 @@ const Navbar = () => {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            <a className="nav-link" href="#">Product <span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
+            <a className="nav-link" href="#">Company</a>
           </li>
           <li className="nav-item dropdown">
             <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown
+              Connect
             </a>
             <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a className="dropdown-item" href="#">Action</a>
-              <a className="dropdown-item" href="#">Another action</a>
-              <div className="dropdown-divider"></div>
-              <a className="dropdown-item" href="#">Something else here</a>
+              <a className="dropdown-item" href="#">Contact</a>
+              <a className="dropdown-item" href="#">Newsletter</a>
+              <a className="dropdown-item" href="#">LinkedIn</a>
             </div>
           </li>
-          <li className="nav-item">
-            <a className="nav-link disabled" href="#">Disabled</a>
-          </li>
         </ul>
-        <form className="form-inline my-2 my-lg-0">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+        <hr></hr>
+        <p>Login</p>
+        <button>Sign Up</button>
       </div>
     </nav>
   )
